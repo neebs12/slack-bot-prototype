ccdb1e268132ba2f29ba7fe64a9fd70b529caa53
Jason Aricheta <jason.aricheta@gmail.com>
2023-03-18 11:12:39 +1300

    have applied threaded context

--- open-ai-interface.js
+++ open-ai-interface.js
@@ -31,4 +31,47 @@ const generate = async ({ text }) => {
   }
 };
 
-export { generate };
+const generate2 = async (slackMessages, triggerRegex) => {
+  const validMsgArry = constructMessageArray(slackMessages, triggerRegex);
+  const systemObject = {
+    role: "system",
+    content:
+      "You are a socratic tutor, your personality is a helpful teacher that does not give the answer but helps the student to find the answer",
+  };
+  const validMsgArryWithSystem = [systemObject, ...validMsgArry];
+
+  console.log({ validMsgArryWithSystem });
+  // return JSON.stringify(validMsgArryWithSystem);
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: validMsgArryWithSystem,
+    });
+
+    return completion.data.choices[0].message.content;
+    // return completion.data.choices[0].text;
+    // return "valid but processing reponse";
+  } catch (e) {
+    console.log(e);
+    return "Sorry, I don't understand.";
+  }
+};
+
+const constructMessageArray = (messagesArray, triggerRegex) => {
+  const userId = messagesArray[0].user;
+  const botId = (
+    messagesArray.find((message) => message.bot_id) ?? { user: undefined }
+  ).user;
+  // cool! now we have isolated the user and bot ids
+  // we then filter the messages according to the existence of the user and bot ids, ie if theres a second user that is not the first bot OR the parent user, we ignore it
+  const filteredMessages = messagesArray.filter((message) => {
+    return message.user === userId || message.user === botId;
+  });
+  const mappedMessages = filteredMessages.map((message) => {
+    const role = message.user === userId ? "user" : "assistant";
+    return { role, content: message.text.replace(triggerRegex, "").trim() };
+  });
+  return mappedMessages;
+};
+
+export { generate, generate2 };
--- slack-bot.js
+++ slack-bot.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config(); // prior EVERYTHING else otherwise .env arent loaded properly
 
 import slackBolt from "@slack/bolt";
-import { generate } from "./open-ai-interface.js";
+import { generate, generate2 } from "./open-ai-interface.js";
 
 const app = new slackBolt.App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -24,11 +24,49 @@ app.message(/^hello tutor[.!]?/i, async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
   console.log("Hello tutor is triggered with message: ", message.text);
   const before = performance.now();
-  // console.log({ text: message.text });
-  // await say(`Hey there <@${message.user}>!`);
   await say(`${await generate(message)}`);
   const after = performance.now();
   console.log(`Call to generate took ${after - before} milliseconds`);
+});
+
+/*
+There needs to be a few things which happen here:
+- the bot needs to be able to listen to the thread
+- the bot needs to be able to only reply to the person who started the thread (this seems to make the most sense)
+- how to handle multiple conseq messages from a valid user? I guess thats ok.
+- so ideal message structure is:
+{
+  system: "you are a socratic tutor, your personality is a helpful teacher that does not give the answer but helps the student to find the answer",
+  user1: `{first message}`,
+  agent: `{agent message}`, etc.
+}
+- we need to be aware of the token limitations of this bot, so this needs to be considered somehow.
+- need to check if the message is from a user or a bot
+*/
+
+const triggerRegex = /^tutor:[.!]?/i;
+app.message(triggerRegex, async ({ message, say }) => {
+  // dont reply to bot messages
+  if (message.subtype === "bot_message") return;
+  let generatedReply;
+  if (message.thread_ts) {
+    // get all the converstations in the current thread where the message was sent
+    const { messages } = await app.client.conversations.replies({
+      channel: message.channel,
+      ts: message.thread_ts,
+    });
+    // console.log({ messages: messages.map((m) => m.text) });
+    generatedReply = await generate2(messages, triggerRegex);
+  } else {
+    // so the generated reply here will be a normal generation message
+    generatedReply = await generate2([message], triggerRegex);
+  }
+
+  await say({
+    text: `Bot message is: ${generatedReply}`,
+    thread_ts: message.ts,
+    reply_broadcast: false, // dont post reply to channel
+  });
 });
 
 (async () => {
